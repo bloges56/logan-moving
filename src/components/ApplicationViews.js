03829326ab8fda78a18fc2c5c424302101bb14c9
Brady Logan <brady.drew.logan@gmail.com>
2020-10-16 15:34:20 -0500

    can login but getting warnings from using modal

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
+import { Login } from "./auth/Login"
 
 export const ApplicationViews = props => {
     return (
         <>
+
+            <Route exact path="/login">
+                <Login />
+            </Route>
 
             <Route exact path="/">
                 <Home />
