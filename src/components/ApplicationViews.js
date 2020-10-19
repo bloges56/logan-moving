c12ed72993317b4c3d5d989b6f49816d0b2fe6fe
Brady Logan <brady.drew.logan@gmail.com>
2020-10-19 09:48:12 -0500

    application now redirects the user to the login page if they are not logged into an account

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,20 +1,10 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
-import { Login } from "./auth/Login"
-import { Register } from "./auth/Register"
 
 export const ApplicationViews = props => {
     return (
         <>
-
-            <Route exact path="/login">
-                <Login />
-            </Route>
-
-            <Route exact path="/register">
-                <Register />
-            </Route>
 
             <Route exact path="/">
                 <Home />
--- src/components/LoganMoving.js
+++ src/components/LoganMoving.js
@@ -1,8 +1,28 @@
 import React from "react"
+import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 export const LoganMoving = () => (
     <>
-        <ApplicationViews />
+        <Route render={() => {
+            if (localStorage.getItem("current_user")) {
+                return (
+                    <>
+                        <ApplicationViews />  
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+
+        <Route path="/login">
+			<Login />
+        </Route>
+        <Route path="/register">
+			<Register />
+        </Route>
     </>
 )
