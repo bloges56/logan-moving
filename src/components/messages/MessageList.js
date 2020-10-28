752eb2496d16f11ec930841c13096fed83c1e927
Brady Logan <brady.drew.logan@gmail.com>
2020-10-28 15:58:37 -0500

    cleaned up code

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -50,7 +50,6 @@ export const MessageList = () => {
             .then(() => {
                 setShowDisplayOnlyMessage(true)
                 setIsLoading(false)
-                setSelectedUser(selectedUser)
             })
         }
     }
@@ -72,7 +71,6 @@ export const MessageList = () => {
                     public: selectedUser.id !== undefined
                 })
                 setIsLoading(false)
-                setSelectedUser(selectedUser)
             })
         }   
     }
@@ -83,22 +81,19 @@ export const MessageList = () => {
         let response = await fetch("http://localhost:8088/messages")
 
         if (response.status == 502) {
-            // Status 502 is a connection timeout error,
-            // may happen when the connection was pending for too long,
-            // and the remote server or a proxy closed it
-            // let's reconnect
+            
             await getUpdate();
+
           } else if (response.status != 200) {
-            // An error - let's show it
-            // showMessage(response.statusText);
-            // Reconnect in one second
+
             await new Promise(resolve => setTimeout(resolve, 1000));
             await getUpdate();
+
           } else {
-            // Get and show the message
+            
             let message = await response.text();
             setUpdate(message);
-            // Call subscribe() again to get the next message
+            
             await getUpdate();
           }
     }
