e4e80c7dac69d74a85e4958d499952f92fa5ee35
Brady Logan <brady.drew.logan@gmail.com>
2020-11-09 13:31:33 -0600

    styling of employee details

--- src/components/employees/EmployeeDetail.js
+++ src/components/employees/EmployeeDetail.js
@@ -15,14 +15,24 @@ export const EmployeeDetail = () => {
                     <Col>
                         <h2>{selectedEmployee.firstName + " " + selectedEmployee.lastName}</h2>
                     </Col>
-                    <Col>
-                        <Link to={`/employees/edit/${selectedEmployee.id}`}>Edit</Link>
+                    <Col xs="2">
+                        <Link to={`/employees/edit/${selectedEmployee.id}`}><Button color="warning">Edit</Button></Link>
                     </Col>
                     <Col>
                         <Button color="danger" onClick={event => {
                             event.preventDefault()
                             fireEmployee(selectedEmployee.id)
                         }}>Fire</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h4>Date Hired: {new Date(selectedEmployee.dateHired).toLocaleDateString()}</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <h4>Phone: {selectedEmployee.phone}</h4>
                     </Col>
                 </Row>
             </Container>
